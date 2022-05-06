aedd2ec9d293140f3809866b4e0f2d5d33fbeeb2
luochun <luochun15@gmail.com>
2022-05-06 23:04:38 +0800

    显示个人资料

--- app/Models/User.php
+++ app/Models/User.php
@@ -23,6 +23,7 @@ class User extends Authenticatable implements MustVerifyEmailContract
         'name',
         'email',
         'password',
+        'introduction',
     ];
 
     /**
--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -5,13 +5,14 @@
  <div class="col-lg-3 col-md-3 hidden-sm hidden-xs user-info">
  <div class="card ">
  {{-- <img class="card-img-top" src="#"> --}}
- <div class="card-body">
- <h5><strong>个人简介</strong></h5>
- <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. </p>
- <hr>
- <h5><strong>注册于</strong></h5>
- <p>January 01 1901</p>
- </div>
+  <div class="card-body">
+    <h5><strong>个人简介</strong></h5>
+    <p>{{ $user->introduction }}</p>
+    <hr>
+    <h5><strong>注册于</strong></h5>
+    <p>{{ $user->created_at->diffForHumans() }}</p>
+  </div>
+
  </div>
  </div>
  <div class="col-lg-9 col-md-9 col-sm-12 col-xs-12">
