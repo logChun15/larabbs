fce3de0b6620a38ff95904920fead788118d7ff6
luochun <luochun15@gmail.com>
2022-05-07 19:22:36 +0800

    话题列表页面

--- app/Models/Topic.php
+++ app/Models/Topic.php
@@ -9,4 +9,16 @@ class Topic extends Model
     use HasFactory;
 
     protected $fillable = ['title', 'body', 'user_id', 'category_id', 'reply_count', 'view_count', 'last_reply_user_id', 'order', 'excerpt', 'slug'];
+
+    public function category()
+    {
+    return $this->belongsTo(Category::class);
+    }
+    public function user()
+    {
+    return $this->belongsTo(User::class);
+    }
+    // 有了以上的关联设定，后面开发中我们可以很方便地通过 $topic->category 、 $topic->user 来获取到话题对应的
+    // 分类和作者。
+
 }
--- resources/views/topics/_sidebar.blade.php
+++ resources/views/topics/_sidebar.blade.php
@@ -1,4 +1,4 @@
-<div class="card ">
+{{-- <div class="card ">
   <div class="card-body">
     <a href="{{ route('topics.create') }}" class="btn btn-success w-100" aria-label="Left Align">
       <i class="fas fa-pencil-alt mr-2"></i> 新建帖子
@@ -39,4 +39,10 @@
       @endforeach
     </div>
   </div>
-@endif
+@endif --}}
+
+<div class="card ">
+  <div class="card-body">
+  右边导航栏
+  </div>
+</div>
