0e931f092255e03af40faa5248b8ae5140bcbd95
luochun <luochun15@gmail.com>
2022-05-07 22:26:20 +0800

    话题排序

--- app/Http/Controllers/Api/TopicsController.php
+++ app/Http/Controllers/Api/TopicsController.php
@@ -14,10 +14,10 @@ class TopicsController extends Controller
         $this->middleware('auth', ['except' => ['index', 'show']]);
     }
 
-	public function index()
+	public function index(Request $request, Topic $topic)
 	{
 		// $topics = Topic::paginate(30);
-        $topics = Topic::with('user', 'category')->paginate(30); //with解决n+1请求问题
+        $topics = $topic->withOrder($request->order)->paginate(20); //with解决n+1请求问题
         // 方法 with() 提前加载了我们后面需要用到的关联属性 user 和 category ，并做了缓存。后面即使是在遍历数据
         // 时使用到这两个关联属性，数据已经被预加载并缓存，因此不会再产生多余的 SQL 查询：
 
@@ -61,4 +61,6 @@ class TopicsController extends Controller
 
 		return redirect()->route('topics.index')->with('message', 'Deleted successfully.');
 	}
+
+
 }
