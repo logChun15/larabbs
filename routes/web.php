d07041815935eaeff9dcb43ed73a4a77ee48a41e
luochun <luochun15@gmail.com>
2022-05-09 23:20:36 +0800

    消息通知列表

--- routes/web.php
+++ routes/web.php
@@ -45,3 +45,4 @@ Route::get('topics/{topic}/{slug?}', 'Api\TopicsController@show')->name('topics.
 // Route::resource('notifications', 'NotificationsController', ['only' => ['index']]);
 // Route::get('permission-denied', 'PagesController@permissionDenied')->name('permission-denied');
 Route::resource('replies', 'Api\RepliesController', ['only' => [ 'store', 'destroy']]);
+Route::resource('notifications', 'NotificationsController', ['only' => ['index']]);
