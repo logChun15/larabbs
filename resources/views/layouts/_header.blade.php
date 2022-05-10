db2786d9c24b9d383d7968a641e47b30b888030a
luochun <luochun15@gmail.com>
2022-05-10 19:08:00 +0800

    集成administrator

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -48,6 +48,18 @@
           <a class="dropdown-item" href="{{ route('root') }}">首页</a>
           <a class="dropdown-item" href="{{ route('users.show',Auth::id()) }}">个人中心</a>
           <a class="dropdown-item" href="{{ route('users.edit',Auth::id()) }}">编辑资料</a>
+          @can('manage_contents')
+            <a class="dropdown-item" href="{{ url(config('administrator.uri')) }}">
+            <i class="fas fa-tachometer-alt mr-2"></i>
+            管理后台
+            </a>
+            <div class="dropdown-divider"></div>
+            @endcan
+            <a class="dropdown-item" href="{{ route('users.show', Auth::id()) }}">
+            <i class="far fa-user mr-2"></i>
+            个人中心
+            </a>
+
         <div class="dropdown-divider"></div>
           <a class="dropdown-item" id="logout" href="#">
             <form action="{{ route('logout') }}" method="POST">
